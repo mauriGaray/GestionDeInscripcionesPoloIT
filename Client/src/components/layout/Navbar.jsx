64b412582cad47587e12d4751928b154465a67de
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-10-06 23:40:03 -0300

    se modificó la el nav

--- Client/src/components/layout/Navbar.jsx
+++ Client/src/components/layout/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     <nav className="bg-indigo-100">
       <div className="mx-auto max-w-7xl px-2 lg:px-6 sm:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
+          <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
             <button
               id="menuButton"
               type="button"
@@ -32,53 +32,48 @@ export const Navbar = () => {
               </svg>
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <img className="h-8 w-auto" src={Logo} alt="Logo" />
-            </div>
-            <div className="hidden lg:ml-6 lg:block">
+          <div className="flex flex-1 items-center justify-center md:justify-start lg:items-stretch ">
+            <Link to={"/"}>
+              <div className="flex flex-shrink-0 items-center">
+                <img
+                  className="h-8 w-auto md:mr-8 lg:mr-16"
+                  src={Logo}
+                  alt="Logo"
+                />
+              </div>
+            </Link>
+            <div className="hidden md:flex lg:block ">
               <div className="flex space-x-4">
                 <li className="text-white list-none">
                   <Link to={"/"}>
-                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                      Inicio
+                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-bold text-base">
+                      Proyectos en proceso
                     </button>
                   </Link>
                 </li>
                 <li className="text-white list-none">
                   <Link to={"/"}>
-                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                      Cursos
+                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-bold text-base">
+                      Nosotros
                     </button>
                   </Link>
                 </li>
                 <li className="text-white list-none">
                   <Link to={"/"}>
-                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                      Perfil
-                    </button>
-                  </Link>
-                </li>
-                <li className="text-white list-none">
-                  <Link to={"/"}>
-                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                      Mentor
+                    <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-bold text-base">
+                      Contacto
                     </button>
                   </Link>
                 </li>
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
-            <div className="relative ml-3">
+          <div className="hidden lg:flex absolute inset-y-0 right-0 items-center pr-2 font-bold text-lg md:flex  lg:static lg:inset-auto lg:ml-6 lg:pr-0">
+            <button className="relative ml-3 text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-bold text-base">
               <li className="list-none">
-                <Link to={"/"}>
-                  <button className="text-gray-800 px-4 py-2 rounded flex items-center">
-                    <img src={Profile} alt="perfil" />
-                  </button>
-                </Link>
+                <Link to={"/login"}>Iniciar sesión</Link>
               </li>
-            </div>
+            </button>
           </div>
         </div>
       </div>
@@ -90,29 +85,22 @@ export const Navbar = () => {
           <div className="space-y-1 px-2 pb-3 pt-2">
             <li className="text-white list-none">
               <Link to={"/"}>
-                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                  Inicio
+                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2  font-bold text-base">
+                  Proyectos en proceso
                 </button>
               </Link>
             </li>
             <li className="text-white list-none">
               <Link to={"/"}>
-                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                  Cursos
+                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2  font-bold text-base">
+                  Nosotros
                 </button>
               </Link>
             </li>
             <li className="text-white list-none">
               <Link to={"/"}>
-                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                  Perfil
-                </button>
-              </Link>
-            </li>
-            <li className="text-white list-none">
-              <Link to={"/"}>
-                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                  Mentor
+                <button className="text-neutral-950 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2  font-bold text-base">
+                  Contacto
                 </button>
               </Link>
             </li>
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -9,9 +9,17 @@ export default {
       },
     },
     screens: {
-      sm: { max: "650px" },
-      // 'md': {'min': '768px', 'max': '1023px'},
-      lg: { min: "651px" },
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
     },
   },
   plugins: [],

40bc18d3975113ac464c05666db9bce2375df70f
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-10-08 23:18:25 -0300

    Soporte de imagenes V0

---
+++ Server/src/routes/imageRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const imageController = require("../controllers/imageController");
+
+router.post(
+  "/upload",
+  imageController.uploadMiddleware,
+  imageController.uploadImage
+);
+
+module.exports = router;
